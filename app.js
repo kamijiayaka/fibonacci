9b4642be8ba1ecbc612b20d5725bdbe73050508e
Ayaka <a.kamiji@ultinet.co.jp>
2019-07-23 11:24:17 +0900

    フィボナッチ作成

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+let result = 1;
+var fibMap = new Map();
+fibMap.set(0,0);
+fibMap.set(1,1);
+
+
+function fib(n) {
+if(n>=2){
+    result=fibMap.get(n-1)+fibMap.get(n-2);
+    fibMap.set(n,result);
+  }else if(n===1||n===0){
+    result= fibMap.get(n)
+  }
+  return result;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
